ff29bfb27a5288746396c3fa2caaab9e469da729
dvtkachenko <smartphone000014@gmail.com>
2018-07-30 18:45:38 +0200

    models changes

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from './model/item.model';
+import { Item } from '../models/item.model';
 
 @Component({
   selector: 'app-items',
